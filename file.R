b1aac8dfcfadbc4332a8b446bb2df47c803032c7
neko3240 <neko3240@gmail.com>
2019-04-12 19:29:31 -0400

    0.01

--- file.R
+++ file.R
@@ -1,3 +1,34 @@
+#rm(list = ls())
+#rm(variableDos)
 #instalar paquete ggplot2
-install.packages("ggplot2")
+#install.packages("ggplot2")
 library(ggplot2)
+
+#Variables
+
+variableUno <- 1
+variableDos <- 5
+variableTres <- 1.6
+
+variableUno <- 'a'
+variableDos <- TRUE
+variableTres <- 'fui i volvi'
+
+
+#Esto borra weas
+rm(variableTres)
+
+#traspasar los datos de distintas variables
+variableUno <- variableDos
+
+variableCuatro <- (variableDos < variableUno) || (variableDos > variableTres)
+
+#comparacion logica
+FALSE || TRUE
+
+#AND logica
+FALSE && TRUE
+variableUno <- variableUno + 1
+
+
+
---
+++ script.R
@@ -0,0 +1,7 @@
+funcion1 <- function(){
+#aqui va el desarroyo de la funcion
+
+print("Hola Mundo") 
+  
+}
+
