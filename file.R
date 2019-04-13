db35dd00c9fddb03d8c1f260947f579b6e32bf40
neko3240 <neko3240@gmail.com>
2019-04-13 11:04:21 -0400

    0.03

--- file.R
+++ file.R
@@ -7,20 +7,15 @@ library(ggplot2)
 #Variables
 
 variableUno <- 1
-variableDos <- 5
-variableTres <- 1.6
-
-variableUno <- 'a'
-variableDos <- TRUE
-variableTres <- 'fui i volvi'
-
+variableDos <- 1.6
+variableTres <- 'a'
+variableCuatro <- TRUE
 
 #Esto borra weas
 rm(variableTres)
 
 #traspasar los datos de distintas variables
 variableUno <- variableDos
-
 variableCuatro <- (variableDos < variableUno) || (variableDos > variableTres)
 
 #comparacion logica
@@ -31,7 +26,12 @@ FALSE && TRUE
 variableUno <- variableUno + 1
 
 source("script.R")
-funcion1()
+Variable1 <- funcion1(1,2)  
 
+source("script.R")
+vel <- funcionVelocidad(10,2)
+
+source("script.R")
+funcionresto(3,4,7)
 
 
--- script.R
+++ script.R
@@ -1,7 +1,26 @@
-funcion1 <- function(){
+funcion1 <- function(x,y){
 #aqui va el desarroyo de la funcion
 
-print("Hola Mundo") 
   
+  #print muestra cosas, texos o reultados
+  print("Hola Mundo") 
+
+  #tambien ejercicios
+  print(x+y)
 }
 
+
+# desarrollo de la velosidad de un cuerpo en relacion al tiempo
+#funcion velocidad (double,double) -> Double
+#test velocidad (10,2) -> 5
+
+funcionVelocidad <- function(d,t){
+v <- d/t
+print(v)
+}
+
+funcionresto <-function(a,b,c){
+sol <- trunc((a*b)/7)  
+print(sol)
+  
+}
